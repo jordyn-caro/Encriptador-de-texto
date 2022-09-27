75f5c718e8bc325bf4491c689dd6112e455a5137
Richard <richardcarogarrido@gmail.com>
2022-09-26 22:30:02 -0500

    Archivos para la prueba 1

---
+++ challengue-1/assets/js/copiar.js
@@ -0,0 +1,8 @@
+function copiarPortapapeles() {
+    var copiado = document.getElementById("resultado");
+    copiado.select();
+    document.execCommand('copy');
+
+}
+
+botonCopiar.onclick = copiarPortapapeles;
